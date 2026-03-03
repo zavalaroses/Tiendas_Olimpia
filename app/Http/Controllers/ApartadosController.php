c304ef7c60c818622aa1e23d68508f3d2936a4ef
zavalaroses <zavalaroses@gmail.com>
2026-03-03 14:48:00 -0600

    hot fix apartado muebles deleted_at

--- app/Http/Controllers/ApartadosController.php
+++ app/Http/Controllers/ApartadosController.php
@@ -269,7 +269,7 @@ class ApartadosController extends Controller
                         'am.id_mueble',
                         'am.cantidad',
                     )
-                    ->whereNull('apartado_muebles.deleted_at')
+                    ->whereNull('am.deleted_at')
                     ->where('apartados.id',$request->id_apartado)
                 ->get(); 
               
