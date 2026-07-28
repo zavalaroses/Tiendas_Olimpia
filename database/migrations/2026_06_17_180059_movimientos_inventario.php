9abfe1a125e7cdea3ffd0e2a3e17af0d773dfe85
zavalaroses <zavalaroses@gmail.com>
2026-07-28 10:41:26 -0600

    fix: migracion movimientos inventario

--- database/migrations/2026_06_17_180059_movimientos_inventario.php
+++ database/migrations/2026_06_17_180059_movimientos_inventario.php
@@ -11,33 +11,33 @@ return new class extends Migration
      */
     public function up(): void
     {
-        schema::create('movimientos_inventario', function (Blueprint $table){
-            $table->id();
-            $table->foreignId('tienda_id')->constrained();
-            $table->foreignId('mueble_id')->constrained();
-            $table->enum('tipo',[
-                'compra',
-                'venta',
-                'apartado',
-                'cancelacion_apartado',
-                'entrega',
-                'ajuste_entrada',
-                'ajuste_salida',
-                'garantia_entrada',
-                'garantia_salida',
-            ]);
-            $table->integer('cantidad');
-            $table->integer('cantidad_movimiento');
-            $table->decimal('costo_unitario',12,2)->nullable();
-            $table->string('referencia_tipo')->nullable();
-            $table->date('fecha_movimiento');
-            $table->timestamps();
-            $table->index([
-                'tienda_id',
-                'mueble_id',
-                'fecha_movimiento'
-            ]);
-        });
+        // schema::create('movimientos_inventario', function (Blueprint $table){
+        //     $table->id();
+        //     $table->foreignId('tienda_id')->constrained();
+        //     $table->foreignId('mueble_id')->constrained();
+        //     $table->enum('tipo',[
+        //         'compra',
+        //         'venta',
+        //         'apartado',
+        //         'cancelacion_apartado',
+        //         'entrega',
+        //         'ajuste_entrada',
+        //         'ajuste_salida',
+        //         'garantia_entrada',
+        //         'garantia_salida',
+        //     ]);
+        //     $table->integer('cantidad');
+        //     $table->integer('cantidad_movimiento');
+        //     $table->decimal('costo_unitario',12,2)->nullable();
+        //     $table->string('referencia_tipo')->nullable();
+        //     $table->date('fecha_movimiento');
+        //     $table->timestamps();
+        //     $table->index([
+        //         'tienda_id',
+        //         'mueble_id',
+        //         'fecha_movimiento'
+        //     ]);
+        // });
     }
 
     /**
